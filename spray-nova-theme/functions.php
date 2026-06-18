c8c0547098165a577e138eecbfef370cf9ade509
JorgeGNDR <jgandarasanchis@gmail.com>
2026-06-19 00:45:14 +0200

    Improve mobile shop product width

--- spray-nova-theme/functions.php
+++ spray-nova-theme/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SPRAY_NOVA_VERSION', '1.3.14' );
+define( 'SPRAY_NOVA_VERSION', '1.3.15' );
 
 require_once get_template_directory() . '/inc/customizer.php';
 
